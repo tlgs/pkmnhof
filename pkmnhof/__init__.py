e869b498a4d24917733f0c70a9925ede55df85a9
Tiago Seabra <tlgs@users.noreply.github.com>
2021-09-13 19:42:54 +0100

    refactor: move CLI to __main__.py

--- pkmnhof/__init__.py
+++ pkmnhof/__init__.py
@@ -1,77 +1,24 @@
-import argparse
 import importlib.metadata
 import importlib.resources
 import tarfile
 
-from PIL import Image, ImageShow
-
+__all__ = ["Pokedex"]
 __version__ = importlib.metadata.version("pkmnhof")
 
 
 class Pokedex:
-    def __init__(self, gen):
+    def __init__(self, gen=1):
         self.gen = gen
-        self._tar = self._load_archive(gen)
+        self._tar = self._load_archive()
         self.frame = self._tar.extractfile("frame.png")
 
     def __getitem__(self, key):
         return self._tar.extractfile(f"{key:03d}.png")
 
-    def _load_archive(self, gen):
+    def _load_archive(self):
         m = {1: "rb"}
 
         pkg = importlib.resources.files("pkmnhof")
-        archive = tarfile.open(pkg / "data" / f"{m[gen]}.tar.gz", mode="r:gz")
+        archive = tarfile.open(pkg / "data" / f"{m[self.gen]}.tar.gz", mode="r:gz")
 
         return archive
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("numbers", nargs=6, type=int, metavar="N")
-    parser.add_argument("-o", "--output")
-    parser.add_argument("-r", "--resize", default=1, type=float)
-    parser.add_argument("--no-frame", action="store_true")
-    parser.add_argument(
-        "--version", action="version", version=f"pkmnhof, version {__version__}"
-    )
-    args = parser.parse_args()
-
-    if not all(0 < x < 152 for x in args.numbers):
-        raise ValueError
-
-    if args.resize is not None and not (1 <= args.resize <= 3):
-        raise ValueError
-
-    pokedex = Pokedex(1)
-
-    pad = int(9 * args.resize)
-    side = int(60 * args.resize)
-    tmp = Image.new(mode="RGBA", size=(pad * 2 + side * 6, pad * 2 + side))
-    for i, n in enumerate(args.numbers):
-        tmp.paste(
-            Image.open(pokedex[n]).resize((side, side)), box=(pad + side * i, pad)
-        )
-
-    if args.no_frame:
-        background = Image.new(
-            mode="RGBA", size=(pad * 2 + side * 6, pad * 2 + side), color="#ffffff"
-        )
-    else:
-        background = (
-            Image.open(pokedex.frame)
-            .convert("RGBA")
-            .resize((pad * 2 + side * 6, pad * 2 + side))
-        )
-
-    final = Image.alpha_composite(background, tmp)
-
-    if args.output is not None:
-        final.save(args.output)
-    else:
-        ImageShow.register(ImageShow.EogViewer, 0)  # prefer `eog` over `display`
-        ImageShow.show(final)
-
-
-if __name__ == "__main__":
-    main()
--- pkmnhof/__main__.py
+++ pkmnhof/__main__.py
@@ -1,3 +1,53 @@
-from pkmnhof import main
+import argparse
 
-main()
+from PIL import Image, ImageShow
+
+from pkmnhof import Pokedex, __version__
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("numbers", nargs=6, type=int, metavar="N")
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-r", "--resize", default=1, type=float)
+    parser.add_argument("--no-frame", action="store_true")
+    parser.add_argument(
+        "--version", action="version", version=f"pkmnhof, version {__version__}"
+    )
+    args = parser.parse_args()
+
+    if args.resize is not None and not (1 <= args.resize <= 3):
+        raise ValueError
+
+    pokedex = Pokedex()
+
+    pad = int(9 * args.resize)
+    side = int(60 * args.resize)
+    tmp = Image.new(mode="RGBA", size=(pad * 2 + side * 6, pad * 2 + side))
+    for i, n in enumerate(args.numbers):
+        tmp.paste(
+            Image.open(pokedex[n]).resize((side, side)), box=(pad + side * i, pad)
+        )
+
+    if args.no_frame:
+        background = Image.new(
+            mode="RGBA", size=(pad * 2 + side * 6, pad * 2 + side), color="#ffffff"
+        )
+    else:
+        background = (
+            Image.open(pokedex.frame)
+            .convert("RGBA")
+            .resize((pad * 2 + side * 6, pad * 2 + side))
+        )
+
+    final = Image.alpha_composite(background, tmp)
+
+    if args.output is not None:
+        final.save(args.output)
+    else:
+        ImageShow.register(ImageShow.EogViewer, 0)  # prefer `eog` over `display`
+        ImageShow.show(final)
+
+
+if __name__ == "__main__":
+    main()
