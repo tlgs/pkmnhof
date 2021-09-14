874dd9e26f76685f4ab884bc8c9b528257e9e4e1
Tiago Seabra <tlgs@users.noreply.github.com>
2021-09-14 13:09:09 +0100

    refactor: rename _tar to _archive

--- pkmnhof/__init__.py
+++ pkmnhof/__init__.py
@@ -9,13 +9,14 @@ __version__ = importlib.metadata.version("pkmnhof")
 class Pokedex:
     def __init__(self, gen=1):
         self.gen = gen
-        self._tar = self._load_archive()
-        self.frame = self._tar.extractfile("frame.png")
+
+        self._archive = self._open_archive()
+        self.frame = self._archive.extractfile("frame.png")
 
     def __getitem__(self, key):
-        return self._tar.extractfile(f"{key:03d}.png")
+        return self._archive.extractfile(f"{key:03d}.png")
 
-    def _load_archive(self):
+    def _open_archive(self):
         m = {1: "rb"}
 
         pkg = importlib.resources.files("pkmnhof")
